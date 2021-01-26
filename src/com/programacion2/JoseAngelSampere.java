41ea6d2609c8baeab6c13836675595e4d215625a
JSampereVázquez <jsamperevazquez@danielcastelao.org>
2021-01-26 10:58:19 +0100

    añadida rama colaborador y clase con nombre de colaborador

---
+++ src/com/programacion2/JoseAngelSampere.java
@@ -0,0 +1,11 @@
+package com.programacion2;
+
+/**
+ * Creado por @autor: Usuario
+ * El 26/01/2021
+ **/
+public class JoseAngelSampere {
+    public void verColaborador(){
+        System.out.println("Jose Angel Sampere Vázquez");
+    }
+}
